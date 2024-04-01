316787e4111836baf8541e792f1c1bdbe52a9e0a
Shaiel Vistuch <133270551+ShaielVistuch@users.noreply.github.com>
2024-04-01 09:38:03 +0300

    Delete C++ OOP Course/Exercise 1_ Grade 100 directory

--- C++ OOP Course/Exercise 1_ Grade 100/Header.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdio.h>
-
-int* createAndInput(int& size);
-int pow(int a=1, int b=1);
-float pow(float a, int b);
-double pow(double a, int b);
-int** createAndInput2(int& rows, int& cols);
